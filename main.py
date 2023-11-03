25c82a766af0f16e625d25e6db8d1620f06c7f99
Synel <70210528+Synell@users.noreply.github.com>
2023-11-03 10:15:43 +0100

    fixed an exception

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def load_config() -> tuple[dict, str]:
     if not os.path.isfile('./config.json'):
         print(format_msg('Unable to find the config.json file. Please make sure it is in the same directory as this program.\nIf you don\'t have one yet, you can use the config-sample.json file as a template.', LogType.Error))
         press_any_key()
-        return
+        sys.exit()
 
     with open('./config.json', 'r', encoding = 'utf8') as f:
         config: dict = json.load(f)

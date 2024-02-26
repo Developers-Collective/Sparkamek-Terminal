4d6a0f54736bb41849bd9d79aab68ef060450573
Synel <70210528+Synell@users.noreply.github.com>
2024-02-26 15:04:30 +0100

    lib update

--- data/lib/utils/__init__.py
+++ data/lib/utils/__init__.py
@@ -6,4 +6,6 @@ from .deprecated import deprecated
 from .Singleton import Singleton
 from .StaticClass import StaticClass
 from .Color import Color
+from .AbstractFactory import AbstractFactory
+from .AbstractTypeFactory import AbstractTypeFactory
 #----------------------------------------------------------------------
